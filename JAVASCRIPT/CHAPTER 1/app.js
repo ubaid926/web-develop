cf09dbfec9193d477f4f3791b6d3fdad5a94291e
ubaid926 <s.ghani12390@gmail.com>
2024-10-05 00:31:02 +0500

    update

---
+++ JAVASCRIPT/CHAPTER 1/app.js
@@ -0,0 +1,73 @@
+// chapter 1
+
+// 1
+// alert("hello")
+
+// 2
+// alert("Error! Please Enter a Valid password")
+
+// 3
+// alert("Welcome to JS land... <br/> Happy Coding")
+
+// 4
+// alert("Welcome to JS land...");
+// alert("Happy Coding!");
+
+// 5
+// alert("welcome to website!")
+// alert("hello!")
+// alert("how are you?")
+// alert("hi")
+// alert("thank you!")
+
+// chapter 2
+
+// Q1
+// var username;
+// alert(username);
+
+// Q2
+// var fullName = "ubaidRaza"
+// alert(fullName)
+
+
+// Q3
+// var message="helloworld";
+// alert(message);
+
+
+// Q4
+// var name="ubaidraza"
+// alert(name);
+// var age=24
+// alert(age);
+// var education="matric"
+// alert(education);
+
+
+// Q5
+// var name="PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(name);
+
+
+// Q6
+// var email="ubaidraza350@yahoo.com"
+// alert("my email adress is " +email)
+
+// Q7
+// var book="smarter way to learn javascript"
+// alert("i am trying to learn from the book "+book);
+
+// Q8
+// document.write("yah! I can write HTML content through Javascript")
+
+// Q9
+// var design="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design);
+// document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// CHAPTER:3
+// Q1
+// var age="15 years";
+// alert("I am " +age+ " old");
+
